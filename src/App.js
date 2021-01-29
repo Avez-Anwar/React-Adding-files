7cb9b4c17bf8e3cb4b74b4e87d4bf75b88a59885
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2021-01-29 15:57:51 +0530

    React life Cycle methods

--- src/App.js
+++ src/App.js
@@ -3,58 +3,29 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      items: [],
-      text: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { date: new Date() };
   }
 
-  handleChange(e) {
-    this.setState({ text: e.target.value });
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-
-    if (this.state.text.length === 0) {
-      return;
-    }
-    const newItems = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState((state) => ({
-      items: state.items.concat(newItems),
-      text: ""
-    }));
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
-        <Todo items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            value={this.state.text}
-          />
-          <button>#Add</button>
-        </form>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
-    );
-  }
-}
-
-class Todo extends React.Component {
-  render() {
-    return (
-      <ol>
-        {this.props.items.map(function (item) {
-          return <li>{item.text}</li>;
-        })}
-      </ol>
     );
   }
 }
